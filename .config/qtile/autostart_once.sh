b3125154702a9ca0fa299e5beeb8bae4519eee50
AlexNg <contact@ngjx.org>
2024-08-26 09:25:10 +0800

    feat: Arrage monitors

--- .config/qtile/autostart_once.sh
+++ .config/qtile/autostart_once.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Monitor arrangements
+xrandr --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --output HDMI-1-2 --mode 1920x1080 --right-of eDP-1-1
+
 # Apply wallpaper using wal
 wal -b 282738 -i ~/Wallpaper/Aesthetic2.png &
 
@@ -8,3 +11,4 @@ picom --config ~/.config/picom/picom.conf &
 
 # Start clipster daemon
 clipster -d
+
