e5e29ff90ee8b302186e120a9b0f312a357bf1bd
AlexNg <contact@ngjx.org>
2024-12-02 21:48:18 +0800

    chore(deps): Update nvim deps
    Also updated the import string for codeium

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
     -- AI
-    { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.ai.codeium" },
 
     -- Web Development
     { import = "lazyvim.plugins.extras.lang.typescript" },
