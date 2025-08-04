602818e04cedfad4fa5de99c8770d61bd855a78a
AlexNg <contact@ngjx.org>
2025-08-05 02:34:18 +0800

    fix: Broken venv selector

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -22,9 +22,6 @@ return {
     },
     lazy = false,
     branch = "regexp", -- This is the regexp branch, use this for the new version
-    config = function()
-      require("venv-selector").setup()
-    end,
     keys = {
       { "vs", "<cmd>VenvSelect<cr>" },
     },
