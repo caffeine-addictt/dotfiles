29589f02ed2468cf85ab451c626581442948c5e1
AlexNg <contact@ngjx.org>
2025-09-02 19:53:59 +0800

    chore(deps): Update nvim deps

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -21,7 +21,6 @@ return {
       { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
     },
     lazy = false,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
     keys = {
       { "vs", "<cmd>VenvSelect<cr>" },
     },
