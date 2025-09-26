d4f55c2abc0f793cbd01309d38af9f81a25d154b
AlexNg <contact@ngjx.org>
2025-09-26 22:07:25 +0800

    fix: use master branch

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -18,7 +18,7 @@ return {
       "neovim/nvim-lspconfig",
       "mfussenegger/nvim-dap",
       "mfussenegger/nvim-dap-python", --optional
-      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+      { "nvim-telescope/telescope.nvim", branch = "master", dependencies = { "nvim-lua/plenary.nvim" } },
     },
     lazy = false,
     keys = {
